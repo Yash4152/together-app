a6a820808f25c903e702ea3fc479fd286ae03d66
Yash77 <yashmittal.cse25@jecrc.ac.in>
2024-03-27 00:23:11 +0530

    header for the home page

--- app/header.tsx
+++ app/header.tsx
@@ -3,21 +3,104 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { signIn, signOut, useSession } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DeleteIcon, LogInIcon, LogOutIcon } from "lucide-react";
+import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
+import { useState } from "react";
 
-export function Header(){
-    const session = useSession();
+function AccountDropdown() {
+  const session = useSession();
+  const [open, setOpen] = useState(false);
 
-    return (
-    <header>
-        <div>
-            {session.data? (
-                <Button onClick ={()=> signOut()} >Sign Out</Button>
-            ): (
-                <Button onClick ={()=> signIn("google")}>Sign In</Button>
-            ) }
+  return (
+    <>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant={"link"}>
+            <Avatar className="mr-2">
+              <AvatarImage src={session.data?.user?.image ?? ""} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+
             {session.data?.user?.name}
-            <ModeToggle/>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            onClick={() =>
+              signOut({
+                callbackUrl: "/",
+              })
+            }
+          >
+            <LogOutIcon className="mr-2" /> Sign Out
+          </DropdownMenuItem>
+
+          <DropdownMenuItem
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            <DeleteIcon className="mr-2" /> Delete Account
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
+
+export function Header() {
+  const session = useSession();
+  const isLoggedIn = !!session.data;
+
+  return (
+    <header className="bg-gray-100 py-2 dark:bg-gray-900 z-10 relative">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link
+          href="/"
+          className="flex gap-2 items-center text-xl hover:underline"
+        >
+          <Image
+            src="/icon.png"
+            width="60"
+            height="60"
+            alt="the application icon of a magnifying glass"
+          />
+          Together-app
+        </Link>
+
+        <nav className="flex gap-8">
+          {isLoggedIn && (
+            <>
+              <Link className="hover:underline" href="/browse">
+                Browse
+              </Link>
+
+              <Link className="hover:underline" href="/your-rooms">
+                Your Rooms
+              </Link>
+            </>
+          )}
+        </nav>
+
+        <div className="flex items-center gap-4">
+          {isLoggedIn && <AccountDropdown />}
+          {!isLoggedIn && (
+            <Button onClick={() => signIn()} variant="link">
+              <LogInIcon className="mr-2" /> Sign In
+            </Button>
+          )}
+          <ModeToggle />
         </div>
+      </div>
     </header>
-    );
+  );
 }
