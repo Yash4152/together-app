c873074608279e6a8c0cd35aeac2cec0c63cc296
Yash77 <yashmittal.cse25@jecrc.ac.in>
2024-03-27 13:33:19 +0530

    working on ability to display rooms on home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ModeToggle } from "@/components/mode-toggle";
 import {Providers} from "./provider";
 import { Header } from "./header";
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +21,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>
+        <NextTopLoader />
           <Header/>
           {children}
         </Providers>
